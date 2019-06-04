636b22331d7117a8d4e0221622cd74bd046f7b41
David Williams <davo49fb@gmail.com>
2019-06-03 22:16:12 -0400

    Progress

--- src/App.js
+++ src/App.js
@@ -1,35 +1,93 @@
 import React, { Component } from 'react';
-import ContactList from './BeerList.js';
-import beers from './dataset.json';
-
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
+import ABVS from './datasets/abvs.json';
+import PRICES from './datasets/prices.json';
+import MERGED from './datasets/mergedDataset1_70.json';
+import BeerList from './BeerList.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      workingList: MERGED
+    }
+  }
+
+  buildFilter = (func) => {
+    return (beer) => {
+      let newBeer = beer;
+      delete newBeer.weightValue;
+      newBeer.weightValue = func(beer);
+      return newBeer;
+    }
+  }
+
+  applyFilter = (filter) => {
+    let weightedList = [];
+    for(let beer of this.state.workingList) {
+      weightedList.push(filter(beer));
+    }
+    console.log(weightedList);
+    weightedList.sort(function(a, b) {
+      if(a.weightValue < b.weightValue) {
+        return -1;
+      } else if(a.weightValue > b.weightValue) {
+        return 1;
+      }
+      return 0;
+    })
+    console.log(weightedList);
+    this.setState({
+      workingList: weightedList
+    })
+  }
+
+  resetFilter = () => {
+    this.setState({
+      workingList: this.AVBS
+    })
+  }
+
+  mergeDatasets = () => {
+    let merged = [];
+    let workingPList = PRICES.slice(0);
+    ABVS.forEach((abv, index) => {
+      let mostConfident = {};
+      let best = 0;
+      for(let price = 0; price < workingPList.length; price += 1) {
+        let words = abv.Product.toString().split(" ");
+        let count = 0;
+        for(let word in words) {
+          if(workingPList[price].Description.toLowerCase().includes(words[word].toLowerCase())) {
+            count++;
+          }
+        }
+        let confidence = count/words.length + 0.000001/words.length;
+        if(confidence > best) {
+          mostConfident = {
+            ...workingPList[price],
+            ...abv
+          }
+          best = confidence;
+        }
+      }
+      if(best > 0.7) {
+        merged.push(mostConfident);
+      }
+    })
+    console.log(JSON.stringify(merged));
+  }
+
+  doABVDFilter = () => {
+    this.applyFilter(this.buildFilter(BeerList.ABVDFilter()));
+  }
+
   render() {
     return (
       <div>
-        <BeerList beers={beers}/>
-      </div>
-    );
+        <button onClick={this.doABVDFilter}>DO ABV FILTER</button>
+        <BeerList buildFilter={this.buildFilter} applyFilter={this.applyFilter} beers={this.state.workingList}/>
+        <button disabled={true} onClick={this.mergeDatasets}>MERGE DATASETS</button>
+      </div>);
   }
 }
 
--- src/BeerList.js
+++ src/BeerList.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 
 class BeerList extends Component {
+  static ABVDFilter = () => {
+    return (beer) => {
+      return {
+        weightValue: (beer['Units per 100ml']/parseFloat(beer['Price']))
+      }
+    }
+  }
+
   render() {
     return (
       <ol className='contact-list'>
         {
-          this.props.beers.map(beer => {
+          this.props.beers.map((beer, index) => {
             return (
-              <li key={beer.product} className='contact-list-item'>
-                <div>{beer.product}</div>
+              <li key={index} className='contact-list-item'>
+                <p1>{`${beer['Description']}, served as ${beer['Quantity']} ${beer['Quantity Units']} for ${beer['Price']} and ABV ${beer['ABV']}%`}</p1>
+                <h2>{`Score: ${beer['Units per 100ml']/parseFloat(beer['Price'])}`}</h2>
               </li>
             );
           })
@@ -19,4 +28,4 @@ class BeerList extends Component {
 
 }
 
-export default ListContacts
+export default BeerList;
